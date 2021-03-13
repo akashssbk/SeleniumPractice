67fb0fe93a011460cc94c4901ad4a4abb46fa60b
Akash Bagade <akashssbk@gmail.com>
2021-03-13 17:34:54 +1100

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,7 +1,19 @@
-from selenium import webdriver
 import time
 
-driver = webdriver.Chrome()
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
 driver.get("http://www.pccoepune.com/")
 driver.maximize_window()
 
